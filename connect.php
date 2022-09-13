a9309ec2ba1680b1a776aa9c2a4cfab7caa0d31f
Gabriel Muktan <83398939+Gyabhu@users.noreply.github.com>
2022-09-13 05:56:11 +0545

    Add files via upload

--- connect.php
+++ connect.php
@@ -1,23 +1,26 @@
 <?php
 	$name = $_POST['name'];
 	$email = $_POST['email'];
-	$recipename = $_POST['recipe-name'];
+	$recipename = $_POST['recipename'];
 	$recipe= $_POST['recipe'];
-   
-   
-	
-
 	// Database connection
+	
 	$conn = mysqli_connect ('localhost','root','','viva_voce');
 	$sql_query = "INSERT INTO recipe (full_name,email,recipe_name,recipe)
     VALUES ('$name','$email','$recipename','$recipe')";
     if(mysqli_query($conn, $sql_query))
    {
 	echo file_get_contents("thankyou.html");
+	$to = $email;
+	$subject = "Submission Sucessful";
+	$message = "Dear ". $name.", Your Recipe on ".$recipename . " has been recieved, we will be taking a good look at it and uploading it to our website. Thank you for your precious time submitting Recipes. We ought to engage more with you." ; 
+	$header = "From: gyabhumuktan@yahoo.com";         
+	mail ($to,$subject,$message,$header);
    }
     else
    {
 	echo "Error: " .$sql . "" . mysqli_error($conn);
    }
    mysqli_close($conn);
-?>s
+
+?>
